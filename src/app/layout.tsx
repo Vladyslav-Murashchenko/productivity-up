943a016c650a81efd21b3e87766030a29878a797
Vladyslav-Murashchenko <vlad.murashchenko@gmail.com>
2026-01-25 17:10:21 +0200

    remove unnecessary global styles, use dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Productivity Up",
@@ -23,12 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className="dark" data-theme="dark">
+      <body className="bg-background text-foreground">{children}</body>
     </html>
   );
 }
