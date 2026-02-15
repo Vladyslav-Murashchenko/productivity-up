3a03a6a52d2feb2f4c4dfa83a0139c909b9763a1
Vladyslav-Murashchenko <vlad.murashchenko@gmail.com>
2026-02-15 21:35:21 +0200

    add comment about possible replacement for useTemporaryHiddenTaskId

--- src/features/tasks/useTemporaryHiddenTaskId.ts
+++ src/features/tasks/useTemporaryHiddenTaskId.ts
@@ -5,6 +5,7 @@ import { Task } from "@/libs/api/tasks/model";
 /**
  * Prevents a just-finished active task from flashing in the "to-do" list
  * while tasks query is still stale.
+ * Note: this issue can also be solved if combine useTasks and useActiveTaskState to one useLiveQuery
  */
 export const useTemporaryHiddenTaskId = (tasks?: Task[]) => {
   const [temporaryHiddenId, setTemporaryHiddenId] = useState<Task["id"] | null>(
