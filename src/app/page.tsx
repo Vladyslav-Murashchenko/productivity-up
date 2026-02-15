fcb8546dea0238d8093ebac9424ba89ae514deb2
Vladyslav-Murashchenko <vlad.murashchenko@gmail.com>
2026-02-15 18:31:28 +0200

    fix app width on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Tasks } from "@/features/tasks";
 
 export default function Home() {
   return (
-    <div className="w-200 m-auto py-12 px-4">
+    <div className="max-w-200 m-auto py-12 px-4">
       <header className="mb-12">
         <h1 className="text-3xl font-semibold mb-2">Productivity Up</h1>
         <p className="text-muted">Focus on one task at a time</p>
